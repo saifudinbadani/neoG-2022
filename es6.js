91f61f841304f14c7db9d14de4b7d63c973ee889
Saifudin Badani <87968258+saifudinbadani@users.noreply.github.com>
2022-01-13 11:42:50 +0800

    ex 2

---
+++ es6.js
@@ -0,0 +1,19 @@
+// Convert to ES6
+
+
+var aloo = 1;
+if (aloo == 1) {
+   var a = 2;
+   console.log(a);
+}
+console.log(aloo);
+
+// 2
+// var multiply = function(x, y) {
+//     return x * y;
+//   };
+
+const multiply = (x,y) => x*y
+
+console.log(multiply(2,3))
+
